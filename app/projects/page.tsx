9821ff4d1a8a6ddbb5897d3aa80a1dc134744e95
hasithajayasundara <hasithajayasundara1@gmail.com>
2025-01-11 14:13:52 +0100

    cleanup

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <main className="flex w-full flex-col items-center justify-center">
       <Layout className="pt-16">
         <AnimatedText text="Projects" className="mb-16 lg:!text-7xl sm:!mb-8m sm:!text-6xl xs:!text-4xl" />
-        <div className="flex justify-center py-40">
+        <div className="flex justify-center h-screen">
             <h1>Coming soon</h1>
         </div>
       </Layout>
