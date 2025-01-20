aeb577bfeace56b7386874e96c3a988fe25a0704
hasithajayasundara <hasithajayasundara1@gmail.com>
2025-01-20 01:48:55 +0100

    simplify page transition

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { PageTransition } from "./page-transition";
 
@@ -12,14 +11,12 @@ type Props = {
 export const Layout = ({ children, className = "" }: Props) => {
   const path = usePathname();
   return (
-    <AnimatePresence mode="sync">
-      <div
-        key={path}
-        className={`w-full h-full inline-block z-0 bg-light p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}
-      >
-        <PageTransition />
-        {children}
-      </div>
-    </AnimatePresence>
+    <div
+      key={path}
+      className={`w-full h-full inline-block z-0 bg-light p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}
+    >
+      <PageTransition />
+      {children}
+    </div>
   );
 };
--- components/page-transition.tsx
+++ components/page-transition.tsx
@@ -9,7 +9,6 @@ export const PageTransition = () => {
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-40 bg-primary/80"
         initial={{ x: '100%', width: '100%' }}
         animate={{ x: '0%', width: '0%' }}
-        exit={{ x: ['0%', '100%'], width: ['0%', '100%'] }}
         transition={{ duration: 0.7, ease: "easeInOut" }}
       />
       <motion.div
